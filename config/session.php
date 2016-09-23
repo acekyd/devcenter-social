5e55461985c39afefd710a31dc1a46041726747d
Abati Adewale <acekyd01@gmail.com>
2016-09-23 16:18:39 +0100

    updated session

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
     |
     */
 
-    'lifetime' => 120,
+    'lifetime' => 5,
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
