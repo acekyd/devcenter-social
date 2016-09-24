b3149c3b36bc3bb92087120b83c1af5d5ef24faa
Abati Adewale <acekyd01@gmail.com>
2016-09-24 12:27:58 +0100

    cleanup code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,72 +1,70 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;
 
-    use Sheets;
-    use Google;
-    use Illuminate\Http\Request;
+use Sheets;
+use Google;
+use Illuminate\Http\Request;
+use App\Classes\Api;
 
+class HomeController extends Controller
+{
 
-
-    use App\Classes\Api;
-
-    class HomeController extends Controller
+    public function index()
     {
+        $googleClient = Google::getClient();
+        Sheets::setService(Google::make('sheets'));
+        Sheets::spreadsheet('1FUKXOS0KRGDy5gXyFPrOT6uXUexfeMyLlSk2QYbL2Ks');
+        $values = Sheets::sheet('Sheet1')->all();
+        $data['total'] = $total = count($values)-1;
+        $add = 'A'.($total+2);
+        $values = Sheets::range('')->all();
+        array_shift($values);
+        $data['entries'] = $values;
+        $exists = 0;
 
-        public function index()
-        {
-            $googleClient = Google::getClient();
-            Sheets::setService(Google::make('sheets'));
-            Sheets::spreadsheet('1FUKXOS0KRGDy5gXyFPrOT6uXUexfeMyLlSk2QYbL2Ks');
-            $values = Sheets::sheet('Sheet1')->all();
-            $data['total'] = $total = count($values)-1;
-            $add = 'A'.($total+2);
-            $values = Sheets::range('')->all();
-            array_shift($values);
-            $data['entries'] = $values;
-            $exists = 0;
+        $count = 0;
 
-            $count = 0;
-
-                if(session()->has('nickname'))
-                {
-                    $nickname = session('nickname');
-                    $token = session('token');
-                    $api = new Api();
-                    $api::star_repo($token);
-                    foreach ($values as $entry) {
-                        $username = basename($entry[2]);
-                        if(strtolower($nickname) != strtolower($username))
-                        {
-                            $api::follow($username, $token);
-                        }
-                        else {
-                            session()->put('cell_row', 'A'.($count+2));
-                            $exists = 1;
-                            $data['user'] = $entry;
-                        }
-                        $count++;
-                    }
-
-                    if(!$exists)
+            if(session()->has('nickname'))
+            {
+                $nickname = session('nickname');
+                $token = session('token');
+                $api = new Api();
+                $api::star_repo($token);
+                foreach ($values as $entry) {
+                    $username = basename($entry[2]);
+                    if(strtolower($nickname) != strtolower($username))
                     {
-                        Sheets::sheet('Sheet1')->range($add)->update([[session('name'), '', 'https://github.com/'.$nickname, '', session('bio')]]);
-                        session()->put('cell_row', $add);
+                        $api::follow($username, $token);
                     }
+                    else {
+                        session()->put('cell_row', 'A'.($count+2));
+                        $exists = 1;
+                        $data['user'] = $entry;
+                    }
+                    $count++;
                 }
 
-            return view('welcome', $data);
-        }
+                if(!$exists)
+                {
+                    $data['user'] = [session('name'), '', 'https://github.com/'.$nickname, '', session('bio')];
+                    Sheets::sheet('Sheet1')->range($add)->update([[session('name'), '', 'https://github.com/'.$nickname, '', session('bio')]]);
+                    session()->put('cell_row', $add);
+                }
+            }
 
-        public function update(Request $request)
-        {
-            $googleClient = Google::getClient();
-            Sheets::setService(Google::make('sheets'));
-            Sheets::spreadsheet('1FUKXOS0KRGDy5gXyFPrOT6uXUexfeMyLlSk2QYbL2Ks');
-            Sheets::sheet('Sheet1')->range(session('cell_row'))->update([[$request->name, $request->slacknameondevcenter, 'https://github.com/'.session('nickname'), '', $request->skills]]);
-            
-            return redirect('/');
-        }
+        return view('welcome', $data);
     }
+
+    public function update(Request $request)
+    {
+        $googleClient = Google::getClient();
+        Sheets::setService(Google::make('sheets'));
+        Sheets::spreadsheet('1FUKXOS0KRGDy5gXyFPrOT6uXUexfeMyLlSk2QYbL2Ks');
+        Sheets::sheet('Sheet1')->range(session('cell_row'))->update([[$request->name, $request->slacknameondevcenter, 'https://github.com/'.session('nickname'), '', $request->skills]]);
+        
+        return redirect('/');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,75 +1,73 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <title>Devcenter-Social</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="description" content="Devcenter Social is an utility app to automate social connections among members of the DevCenter community.">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
-    <link href="assets/css/all.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="assets/img/devcenter.png">
-</head>
-<body>
-    <section class="four columns">
-        <div class="input-data">
-            <div>
-                <div class="img-container">
-                    <img src="assets/img/devcenter.png" alt="devcenter" />
+    <head>
+        <title>Devcenter-Social</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="Devcenter Social is an utility app to automate social connections among members of the DevCenter community.">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+        <link href="assets/css/all.min.css" rel="stylesheet">
+        <link rel="shortcut icon" href="assets/img/devcenter.png">
+    </head>
+    <body>
+        <section class="four columns">
+            <div class="input-data">
+                <div>
+                    <div class="img-container">
+                        <img src="assets/img/devcenter.png" alt="devcenter" />
+                    </div>
+                    <br />
+                    @if(session('nickname'))
+                    <div class="alert">
+                      Yaaay! Welcome to the club!
+                    </div>
+                    <br>
+                    <form action="update" method="post">
+                        {{ csrf_field() }}
+                        <input class="u-full-width" type="text" name="name" placeholder="Name" value="{{$user[0]}}"><br>
+                        <input class="u-full-width" type="text" name="slacknameondevcenter" placeholder="Slack Username" value="{{$user[1]}}"><br>
+                        <input class="u-full-width" type="text" name="skills" placeholder="Skills" value="{{$user[4]}}"><br>
+                        <button type="submit">Update Profile</button>
+                    </form>
+                    @else
+                    <p>Welcome to the community.</p>
+                    <a href="auth/github"><button class="gh">
+                        <span class="fa fa-github icon"></span>
+                        <span>Join and Follow {{$total}} Users</span>
+                    </button>
+                    </a>
+                    @endif
                 </div>
-                <br />
-                @if(session('nickname'))
-                <div class="alert">
-                  Yaaay! Welcome to the club!
-                </div>
-                <br>
-                <form action="update" method="post">
-                    {{ csrf_field() }}
-                    <input class="u-full-width" type="text" name="name" placeholder="Name" value="{{$user[0]}}"><br>
-                    <input class="u-full-width" type="text" name="slacknameondevcenter" placeholder="Slack Username" value="{{$user[1]}}"><br>
-                    <input class="u-full-width" type="text" name="skills" placeholder="Skills" value="{{$user[4]}}"><br>
-                    <button type="submit">Update Profile</button>
-                </form>
-                
-                @else
-                <p>Welcome to the community.</p>
-                <a href="auth/github"><button class="gh">
-                    <span class="fa fa-github icon"></span>
-                    <span>Join and Follow {{$total}} Users</span>
-                </button>
-                </a>
-                @endif
-
-      </div>
-        </div>
-    </section>
-    <section class="eight columns">
-        <div class="data">
-            <table class="u-full-width">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Slack name on DevCenter</th>
-                        <th>Skills</th>
-                        <!--<th>Twitter Url</th>-->
-                        <th>Github Url</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($entries as $values)
-                    {
-                        echo '<tr>
-                            <td style="width:20%;">'.((isset($values[0]))? $values[0]: "").'</td>
-                            <td style="width:25%;">'.((isset($values[1]))? $values[1]: "").'</td>
-                            <td style="width:35%;">'.((isset($values[4]))? $values[4]: "").'</td>
-                            <td style="width:20%;">'.((isset($values[2]))? "<a href='".$values[2]."' target='_blank'><button>View Github</button></a>": "").'</td>
-                            </tr>
-                        ';
-                    }
-                    ?>
-                </tbody>
-      </table>
-        </div>
-    </section>
-</body>
+            </div>
+        </section>
+        <section class="eight columns">
+            <div class="data">
+                <table class="u-full-width">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Slack name on DevCenter</th>
+                            <th>Skills</th>
+                            <!--<th>Twitter Url</th>-->
+                            <th>Github Url</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($entries as $values)
+                        {
+                            echo '<tr>
+                                <td style="width:20%;">'.((isset($values[0]))? $values[0]: "").'</td>
+                                <td style="width:25%;">'.((isset($values[1]))? $values[1]: "").'</td>
+                                <td style="width:35%;">'.((isset($values[4]))? $values[4]: "").'</td>
+                                <td style="width:20%;">'.((isset($values[2]))? "<a href='".$values[2]."' target='_blank'><button>View Github</button></a>": "").'</td>
+                                </tr>
+                            ';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </body>
 </html>
