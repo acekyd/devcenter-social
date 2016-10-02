72915aaf53cda67b0dfa04d04e1ddd77bd347910
Abati Adewale <acekyd01@gmail.com>
2016-10-02 23:45:28 +0100

    updated link to google doc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Devcenter-Social</title>
+        <title>Devcenter-Square Social</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
