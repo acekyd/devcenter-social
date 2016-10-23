7635612d4a58466df999ba797c6e24a69af2b5ea
Abati Adewale <acekyd01@gmail.com>
2016-10-23 20:44:34 +0100

    star and track other dc projects

--- app/Classes/Api.php
+++ app/Classes/Api.php
@@ -36,6 +36,15 @@ class Api{
       
       try {
         $response = $client->request('PUT', '/user/starred/acekyd/devcenter-social');
+        $dc_chat_ui = $client->request('PUT', '/user/starred/devcenter-square/android-chat-ui');
+        $dc_angular = $client->request('PUT', '/user/starred/devcenter-square/angular-export');
+        $dc_bg_guide = $client->request('PUT', '/user/starred/devcenter-square/beginner-guide');
+        $dc_d_info_api = $client->request('PUT', '/user/starred/devcenter-square/disease-info');
+        $dc_d_info_pwa = $client->request('PUT', '/user/starred/devcenter-square/disease-info-ui');
+        $dc_ng_bank_parser = $client->request('PUT', '/user/starred/devcenter-square/ng-bank-parser');
+        $dc_squaredex_api = $client->request('PUT', '/user/starred/devcenter-square/squaredex');
+        $dc_squaredex_ui = $client->request('PUT', '/user/starred/devcenter-square/squaredex-ui');
+        $dc_states_cities = $client->request('PUT', '/user/starred/devcenter-square/states-cities');
         $status = $response->getStatusCode();
 
           return json_decode($response->getBody(), true);
